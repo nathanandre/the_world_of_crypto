4c72fbabc053edff34f958eb6a83b0fde71a7ca1
nathanandre <nathanandreburns@gmail.com>
2021-05-04 11:16:37 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in The_World_Of_Crypto.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/The_World_Of_Crypto.rb
@@ -0,0 +1,6 @@
+require "The_World_Of_Crypto/version"
+
+module TheWorldOfCrypto
+  class Error < StandardError; end
+  # Your code goes here...
+end
